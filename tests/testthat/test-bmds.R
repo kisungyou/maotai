907628e1bf43f73f5d848a2c74b9b297f164e696
kisungyou <kisungyou@outlook.com>
2022-02-28 23:14:49 -0500

    bmds please

--- tests/testthat/test-bmds.R
+++ tests/testthat/test-bmds.R
@@ -1,13 +1,16 @@
 library(maotai)
 
 test_that("Bayesian MDS works", {
-  data(iris)
-  dat = as.matrix(iris[,1:4])
+  myn = 10
+  myp = 3
   
-  iris2d = maotai::bmds(dat, ndim=2, mc.iter=2, par.step=4, verbose=FALSE)
+  prep_mat = rbind(matrix(rnorm(myn*myp, mean=-2), ncol=myp),
+                   matrix(rnorm(myn*myp, mean= 2), ncol=myp))
+  
+  iris2d = maotai::bmds(prep_mat, ndim=2, mc.iter=2, par.step=4, verbose=FALSE)
   n_rows = base::nrow(iris2d$embed)
   n_cols = base::ncol(iris2d$embed)
   
-  expect_equal(n_rows, base::nrow(dat))
+  expect_equal(n_rows, base::nrow(prep_mat))
   expect_equal(n_cols, 2)
 })
